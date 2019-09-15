617fe698baefb5a5edbbafb65e0a2df210c448b8
AirStair <AirStair@users.noreply.github.com>
2019-09-16 02:15:25 +0300

    Update dostavim-select.js

--- dostavim-select.js
+++ dostavim-select.js
@@ -1,4 +1,10 @@
 /*
+* ChildNode.after() polyfill
+* from: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/after()/after().md
+*/
+(function(x){var o=x.prototype;o.after||(o.after=function(){var e,m=arguments,l=m.length,i=0,t=this,p=t.parentNode,n=Node,s=String,d=document;if(p!==null){while(i<l){((e=m[i]) instanceof n)?(((t=t.nextSibling)!==null)?p.insertBefore(e,t):p.appendChild(e)):p.appendChild(d.createTextNode(s(e)));++i;}}});}(Element));
+
+/*
 * dostavimSelect(selector, name, label)
 */
 function dostavimSelect(selector, name, label) {
